9777625fb665e4c96ac3f34aab07222ed4a360ea
Tim De Pauw <tim@pwnt.be>
2013-02-15 23:56:24 +0100

    More realistic example data

--- util/generate-example-data.php
+++ util/generate-example-data.php
@@ -10,8 +10,11 @@ $num_projects = 5;
 $min_users = 3;
 $max_users = 8;
 $min_commits = 0;
-$max_commits = 100;
+$max_commits = 50;
+$min_adjust = -100;
+$max_adjust = 100;
 
+$diff = $max_commits - $min_commits;
 foreach (range(1, $num_projects) as $project_id) {
     $data = array();
     $user_count = mt_rand($min_users, $max_users);
@@ -19,8 +22,10 @@ foreach (range(1, $num_projects) as $project_id) {
         $user_name = "User $user_id";
         foreach (range(0, 6) as $day) {
             foreach (range(0, 23) as $hour) {
-                $commit_count = mt_rand($min_commits, $max_commits);
-                $data[$user_name][$day][$hour] = $commit_count;
+                $angle = 3 * M_PI / 2 * ($hour + 1) / 24;
+                $commit_count = $min_commits + round(abs(cos($angle)) * $diff);
+                $commit_count += mt_rand($min_adjust, $max_adjust);
+                $data[$user_name][$day][$hour] = max(0, $commit_count);
             }
         }
     }
