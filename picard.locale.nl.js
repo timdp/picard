3d28d4e485245e566c2df3830938bc93f33e348e
Tim De Pauw <tim.depauw@intec.ugent.be>
2013-02-18 16:44:33 +0100

    Adding Dutch locale

---
+++ picard.locale.nl.js
@@ -0,0 +1,8 @@
+PiCard.addLocale("nl", {
+    dayNames:           [ "MA", "DI", "WO", "DO", "VR", "ZA", "ZO" ],
+    summaryTitle:       "Totaal",
+    summaryLabel:       "Commits:",
+    loadingText:        "Bezig met laden …",
+    loadingFailedText:  "De gegevens konden niet worden geladen. Laad de pagina opnieuw.",
+    thousandsSeparator: "."
+});
