6a5456bf9f4c99189fa035886b8b5188775d5ecd
Tim De Pauw <tim@pwnt.be>
2013-02-19 23:15:00 +0100

    Updating example data so more markers are visible

--- util/generate-example-data.php
+++ util/generate-example-data.php
@@ -10,10 +10,12 @@ $num_projects = 5;
 $min_users = 3;
 $max_users = 8;
 $min_commits = 0;
-$max_commits = 50;
-$min_adjust = -100;
-$max_adjust = 100;
+$max_commits = 100;
+$min_adjust = -200;
+$max_adjust = 0;
 $weekend_scale = 2;
+$phase = M_PI / 6;
+$target_path = dirname(__FILE__) . '/../example/data';
 
 $diff = $max_commits - $min_commits;
 foreach (range(1, $num_projects) as $project_id) {
@@ -23,7 +25,7 @@ foreach (range(1, $num_projects) as $project_id) {
         $user_name = "User $user_id";
         foreach (range(0, 6) as $day) {
             foreach (range(0, 23) as $hour) {
-                $angle = 3 * M_PI / 2 * ($hour + 1) / 24;
+                $angle = $phase + 3 * M_PI / 2 * ($hour + 1) / 24;
                 $commit_count = $min_commits + abs(cos($angle)) * $diff;
                 $commit_count += mt_rand($min_adjust, $max_adjust);
                 $commit_count = max(0, $commit_count);
@@ -35,6 +37,6 @@ foreach (range(1, $num_projects) as $project_id) {
             }
         }
     }
-    $file = "../example/data/project$project_id.json";
+    $file = "$target_path/project$project_id.json";
     file_put_contents($file, json_encode($data, JSON_FORCE_OBJECT));
 }
