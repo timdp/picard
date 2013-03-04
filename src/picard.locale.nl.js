1af24cfc7dea60c21b10337aa54b12537b501256
Tim De Pauw <tim.depauw@intec.ugent.be>
2013-03-04 17:29:25 +0100

    Using thin spaces before ellipsis

--- src/picard.locale.nl.js
+++ src/picard.locale.nl.js
@@ -2,7 +2,7 @@ PiCard.addLocale("nl", {
     dayNames:           [ "MA", "DI", "WO", "DO", "VR", "ZA", "ZO" ],
     summaryTitle:       "Totaal",
     summaryLabel:       "Commits:",
-    loadingText:        "Bezig met laden …",
+    loadingText:        "Bezig met laden\u2009…",
     loadingFailedText:  "De gegevens konden niet worden geladen. Laad de pagina opnieuw.",
     thousandsSeparator: "."
 });
